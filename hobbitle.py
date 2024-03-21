1fc0153d07c8735c17c842d1b785cb7d0828c3c8
Andrew <aldortheold@gmail.com>
2024-03-21 15:31:34 +1000

    modified:   hobbitle.py

--- hobbitle.py
+++ hobbitle.py
@@ -219,8 +219,6 @@ while True:
 
     line1_6 = [l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16,
                l17, l18, l19, l20, l21, l22, l23, l24, l25, l26, l27, l28, l29, l30]
-    
-    lineGroups = [line1, line2, line3, line4, line5, line6]
 
     count = 0
 
@@ -279,8 +277,6 @@ while True:
 
     def smaugCall():
         charCall(smaug)
-
-    
 
     def enterCall():
         global count, guess1, guess2, guess3, guess4, guess5, guess6, congrats_label, the_sol_was_label
